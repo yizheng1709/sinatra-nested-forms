67016df07316b20f4035e865b7f242d09a657b24
Eva <yi.zheng1709@gmail.com>
2021-02-21 22:26:45 -0500

    commit tree e44b5ddedbb8790d12674f608754acd77ec42c23

--- app.rb
+++ app.rb
@@ -4,6 +4,25 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do 
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+
+      erb :show
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@all = []
+    def initialize(params)
+        @name = params[:name]
+        @height = params[:height] 
+        @weight = params[:weight]
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
